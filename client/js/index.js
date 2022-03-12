dd12c0d040b6dd18c9a8cf714bb0a53b11949391
BrokeUniStudent <2706469820@qq.com>
2022-03-12 23:22:15 +0000

    Automize post rendering

--- client/js/index.js
+++ client/js/index.js
@@ -1,21 +1,59 @@
-const { response, query } = require('express')
-var express = require('express')
-var app = express()
-var fs = require('fs')
+// const { get } = require("express/lib/response");
 
-// app.use(express.static('client'))
+// document.addEventListener("DOMContentLoaded",() => {
+//     alert("Hi")
+//     fetch('http://127.0.0.1:3000/gallery')
+//     .then(response => console.log(response.text()))
+//     // response.ok
+//     .then(body => {
+//         alert("Hi");
+//         document.getElementById('content').innerHTML=body;
+//         render_post();
+//     })
+//     // .then(get_post())
+//     .catch( (error => alert(error)))
+//  })
 
-app.get('/gallery', function(req,resp){
-    fs.readFile('./index.html',function(err,data){
-        if (err){
-            resp.writeHead(404,{'Content-Type':'text/html'})
-        }
-        else{
-            resp.writeHead(200,{'Content-Type':'text/html'})
-            resp.write(data.toString())
-        }
-    resp.end()
-    })
-})
+// alert('Hi');
 
-app.listen(8000)
+async function get_post(){
+    // alert('Hi')
+    try {
+        let res = await fetch('HTTP://127.0.0.1:3000/gallery/get_post');
+        var response = await res.text();
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+ 
+
+async function render_post(){
+    // alert('Hi')
+    let response = await get_post();
+
+    var people_data = JSON.parse(response);
+    // render all posts
+    const post_container = document.getElementsByClassName('g-3')[0];
+    for (let i=0; i<people_data.length; i++){
+        post_container.innerHTML += '<div class="col">'
+        post_container.innerHTML += '<div class="card shadow-sm">'
+        post_container.innerHTML += '<img class="bd-placeholder-img card-img-top" width="100%" height="225" src="'+people_data[i].ImageURL+'" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></img>'
+        post_container.innerHTML += '<div class="card-body">'
+        post_container.innerHTML += '<p class="card-text">'+people_data[i].About+'</p>'
+        post_container.innerHTML += '<div class="d-flex justify-content-between align-items-center">'
+        post_container.innerHTML += '<div class="btn-group">'
+        post_container.innerHTML += '<button type="button" class="btn btn-sm btn-outline-secondary">View</button>'
+        post_container.innerHTML += '<button type="button" class="btn btn-sm btn-outline-secondary">Like</button>'
+        post_container.innerHTML += '</div>'
+        post_container.innerHTML += '<small class="text-muted">'+people_data[i].Name+'</small>'
+        post_container.innerHTML += '</div>'
+        post_container.innerHTML += '</div>'
+        post_container.innerHTML += '</div>'
+        post_container.innerHTML += '</div>'
+    }
+}
+
+render_post()
--- server/app.js
+++ server/app.js
@@ -2,10 +2,57 @@ const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
 
-app.use(express.static('client/html'));
+app.use(express.static('./../client/'));
 
 app.use(express.json());
 
 const fs = require('fs');
+
+const html_path = "./../client/html/";
+
+const people_data = require('./data/people.json')
+
+app.get('/gallery', function(req,resp){
+    fs.readFile(html_path+'index.html',function(err,data){
+        if (err){
+            resp.writeHead(404,{'Content-Type':'text/html'});
+        }
+        else{
+            resp.writeHead(200,{'Content-Type':'text/html'});
+            resp.write(data.toString());
+        }
+    resp.end();
+    })
+})
+
+app.get('/gallery/get_post', function(req,resp){
+    // resp.writeHead(200,{'Content-Type':'application/json'});
+    // resp.write(people_data);
+    // resp.end();
+    fs.readFile('data/people.json',function(err,data){
+        if (err){
+            resp.writeHead(404,{'Content-Type':'text/html'});
+        }
+        else{
+            resp.writeHead(200,{'Content-Type':'application/json', 'Access-Control-Allow-Origin': 'http://localhost:3000'});
+            resp.write(data.toString());
+        }
+    resp.end();
+    })
+})
+
+app.get('/form', function(req,resp){
+    fs.readFile(html_path+'form.html',function(err,data){
+        if (err){
+            resp.writeHead(404,{'Content-Type':'text/html'})
+        }
+        else{
+            resp.writeHead(200,{'Content-Type':'text/html'})
+            resp.write(data.toString())
+        }
+    resp.end()
+    })
+})
+app.listen(3000)
 
 module.exports = app;
