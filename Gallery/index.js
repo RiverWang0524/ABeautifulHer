5f52c820dcde78d02e081c6a59576d5cc0a7823c
BrokeUniStudent <2706469820@qq.com>
2022-03-12 15:13:34 +0000

    Upload server.js

---
+++ Gallery/index.js
@@ -0,0 +1,21 @@
+const { response, query } = require('express')
+var express = require('express')
+var app = express()
+var fs = require('fs')
+
+app.use(express.static('client'))
+
+app.get('/gallery', function(req,resp){
+    fs.readFile('./index.html',function(err,data){
+        if (err){
+            resp.writeHead(404,{'Content-Type':'text/html'})
+        }
+        else{
+            resp.writeHead(200,{'Content-Type':'text/html'})
+            resp.write(data.toString())
+        }
+    resp.end()
+    })
+})
+
+app.listen(3000)
