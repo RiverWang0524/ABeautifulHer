ffc0fd9e662e9dd1dff66c37c001fe9a2bb8add2
Shelley N <47695024+twixguk@users.noreply.github.com>
2022-03-13 08:52:10 +0000

    loading data onto html

--- client/js/index.js
+++ client/js/index.js
@@ -1,21 +1,47 @@
-const { response, query } = require('express')
-var express = require('express')
-var app = express()
-var fs = require('fs')
+// load data when screen loads
+window.addEventListener('load', async function (event) {
+    event.preventDefault();
+    try {
+        let response = await fetch('http://127.0.0.1:8090/people');
+        let body = await response.json();
+        loadPeople('galleryalbum', body);
+    } catch (error) {
+        alert('Error: ' + error);
+    }
+});
 
-// app.use(express.static('client'))
-
-app.get('/gallery', function(req,resp){
-    fs.readFile('./index.html',function(err,data){
-        if (err){
-            resp.writeHead(404,{'Content-Type':'text/html'})
-        }
-        else{
-            resp.writeHead(200,{'Content-Type':'text/html'})
-            resp.write(data.toString())
-        }
-    resp.end()
-    })
-})
-
-app.listen(8000)
+// load people on site
+function loadPeople (mediumID, json) {
+    let html = '';
+    for (let i = 0; i < json.length; i++) {
+        const id = json[i]['ID'];
+        const name = json[i]['Name'];
+        const bday = json[i]['Birthday'];
+        const country = json[i]['Country'];
+        const dp = json[i]['Image URL'];
+        const about = json[i]['About'];
+        const skills = json[i]['Skills'];
+        const legacy = json[i]['Legacy'];
+        html += '<div class="col"><div class="card h-100"><div class="row d-flex flex-wrap align-items-center g-0"><div class="col-5 text-center"><img src="';
+        html += dp;
+        html += '" alt="Cover image" class="thumbnail"></div><div class="col-7 py-3 ml-6"><div class="card-body"><h5 class="card-title pb-0">';
+        html += name;
+        html += '</h5><p class="card-subtitle mb-2 text-muted">';
+        html += bday;
+        html += '</p><p class="card-subtitle mb-2 text-muted">';
+        html += country;
+        html += '</p></div></div></div><div class="row d-flex flex-wrap align-items-center g-0"><div class="px-3 pb-3 text-center"><ul class="list-group mb-3"><li class="list-group-item list-group-item-secondary fw-bold">Skills:</li>';
+        for (let x of skills) {
+            html += '<li class="list-group-item">';
+            html += x;
+            html += '</li>';
+        };
+        html += '</ul><ul class="list-group mb-3"><li class="list-group-item list-group-item-secondary fw-bold">Legacy:</li><li class="list-group-item">';
+        html += legacy;
+        html += '</li></ul><ul class="list-group mb-2"><button type="button" class="btn btn-outline-dark viewperson" id="';
+        html += id;
+        html += '">Read More</button></ul></div></div></div></div>'
+    };
+    const medium = document.getElementById(mediumID);
+    medium.innerHTML = html;
+};
--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
 
-app.use(express.static('client/html'));
+app.use(express.static('client'));
 
 app.use(express.json());
 
 const fs = require('fs');
+
+// read people list
+app.get('/people', function (req, resp) {
+    fs.readFile('data/people.json', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        resp.status(200).json(JSON.parse(data));
+    });
+});
 
 module.exports = app;
